ea8f60741f9018d5fedf326da2cf4741f64b0ebc
Betsey Johnson <nseo@lls.edu>
2018-07-19 00:10:13 -0700

    deployed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import character from "./character.json"; 
+import './App.css';
+
+class App extends Component {
+  
+  state = {
+    character
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Clicky Game</Title>
+        {this.state.character.map(character => (
+          <CharacterCard
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
